2c5577b06803a738d4bc43342162b36acf7c5e6a
ZephyRemix <hunyaochong@gmail.com>
2024-06-15 22:13:44 +0800

    Add logic to play a single round of rock, paper, scissors

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+let humanScore = 0;
+let computerScore = 0;
 
 // Get computer's choice 
 function getComputerChoice () {
@@ -32,6 +34,70 @@ function getHumanChoice () {
     return humanChoice;
 }
 
-console.log(getComputerChoice());
+function playRound() {
 
-console.log(getHumanChoice());
+    // store human and computer choice
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+
+    // declare an int variable where 
+    // 0: no winner
+    // 1: player win
+    // 2: computer win
+    let winner = getWinner(humanChoice, computerChoice);
+
+    // announce no winner in case of a draw
+    if (!winner) {
+        console.log("No winner");
+    // else update score and announce result if there is a winner
+    } else {
+        updateScore(winner);
+    }
+}
+
+function updateScore(winner) {
+
+    // if human won, increment score and inform player 
+    if (winner === 1) {
+        humanScore += 1;
+        console.log(`You won`);
+    // if computer won, increment score and inform player 
+    } else {
+        computerScore +=1;
+        console.log("Try again!")
+    }
+}
+
+function getWinner(h_choice, c_choice) {
+
+    // declare playerWin as an int variable
+    let winner;
+
+    // battle: if player win = 1; computer win = 2; no winner = 0
+    switch (h_choice) {
+        case "rock": 
+            c_choice == "rock" ? winner = 0 :
+            c_choice == "paper" ? winner = 2 :
+            winner = 1;
+            break;
+        case "paper":
+            c_choice == "rock" ? winner = 1 :
+            c_choice == "paper" ? winner = 0 :
+            winner = 2;
+            break;
+        case "scissors":
+            c_choice == "rock" ? winner = 2 :
+            c_choice == "paper" ? winner = 1 :
+            winner = 0;
+    }
+
+    // return winner as an integer
+    return winner
+}
+
+
+// console.log(`Computer picked ${getComputerChoice()}`);
+
+// console.log(`Human picked ${getHumanChoice()}`);
+
+playRound();
